d5cfb7c337738088216c0b5a4a6be6ec551f39ae
emonhaque1998 <emonhaque.net@gmail.com>
2024-12-30 01:28:24 +0600

    update

--- resources/js/Components/admin/SideBar.jsx
+++ resources/js/Components/admin/SideBar.jsx
@@ -1,14 +1,47 @@
 import ApplicationLogo from "../ApplicationLogo";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
+import { Link } from "@inertiajs/react";
 
 export default function SideBar({ children }) {
+    const [navDropdown, setNavDropdown] = useState(false);
+
+    const navDropdownHandler = (event) => {
+        event.preventDefault();
+        setNavDropdown(!navDropdown);
+    };
+
     return (
         <div className="flex flex-row">
-            <div className="w-2/12 h-screen shadow-md">
+            <div className="w-2/12 h-screen shadow-md sticky top-0 flex flex-col gap-3">
                 <div className="w-full flex justify-center border-b">
                     <ApplicationLogo
                         className="fill-current text-gray-500"
                         width="w-28"
                     />
+                </div>
+                <div>
+                    <ul className="flex flex-col px-3">
+                        <li className="flex flex-row items-center justify-between">
+                            <div className="flex flex-row items-center gap-2 justify-start">
+                                <AiOutlineDashboard className="text-xl" />
+                                <Link
+                                    href=""
+                                    onClick={navDropdownHandler}
+                                    className="text-md"
+                                >
+                                    Dashboard
+                                </Link>
+                            </div>
+                            {navDropdown ? (
+                                <IoIosArrowDown className="font-thin" />
+                            ) : (
+                                <IoIosArrowForward className="font-thin" />
+                            )}
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="w-full">{children}</div>
